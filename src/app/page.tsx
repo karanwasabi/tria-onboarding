702607f3759506beb95c07dd8ed4f0f35aa87385
Karan Wasabi <karan@karanwasabi.com>
2024-06-16 14:28:02 +0530

    feat: add login providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Footer from '@/components/footer';
 import LoginHero from '@/components/login/login-hero';
+import LoginProviders from '@/components/login/login-providers';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <div className="t-container">
         <LoginHero />
+        <LoginProviders />
         <Footer />
       </div>
     </main>
---
+++ src/components/login/login-providers/login-providers.tsx
@@ -0,0 +1,13 @@
+const LoginProviders: React.FC = () => {
+  return (
+    <>
+      <div className="flex w-full grow flex-col items-center justify-end">
+        <div className="flex w-full flex-col rounded-2xl bg-t-light-4"></div>
+      </div>
+    </>
+  );
+};
+
+LoginProviders.displayName = 'LoginProviders';
+
+export default LoginProviders;
