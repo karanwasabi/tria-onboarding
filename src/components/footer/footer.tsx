777056c3eb4441a9087c491bf6970c642783c84f
Karan Wasabi <karan@karanwasabi.com>
2024-06-16 19:33:26 +0530

    style: add footer effects

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,9 +3,11 @@ import Image from 'next/image';
 const Footer: React.FC = () => {
   return (
     <>
-      <footer className="flex w-full items-center justify-center p-3 opacity-60">
-        <Image src={`/svgs/tria-footer.svg`} alt="Tria Footer Logo" width={20} height={20} />
-        <p className="text-14 pl-2.5 font-semibold text-t-gray">Powered by Tria</p>
+      <footer className="flex w-full items-center justify-center p-2">
+        <div className="flex items-center justify-center rounded-xl p-3 opacity-60 transition hover:cursor-pointer hover:bg-t-light-8 hover:opacity-100">
+          <Image src={`/svgs/tria-footer.svg`} alt="Tria Footer Logo" width={20} height={20} />
+          <p className="text-14 pl-2.5 font-semibold text-t-gray">Powered by Tria</p>
+        </div>
       </footer>
     </>
   );
