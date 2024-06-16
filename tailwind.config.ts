ffc7d52d868f852f760d9fac8e88365fb9b385c9
Karan Wasabi <karan@karanwasabi.com>
2024-06-16 12:48:05 +0530

    feat: add main container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      borderRadius: {
+        '20': '20px',
+      },
+      boxShadow: {
+        't-container': '0px 0px 1px 0px #80808033, 0px 2px 30px 0px #80808014, 0px 0px 15px 0px #80808008',
+      },
       colors: {
         't-surface-black': '#101010',
       },
