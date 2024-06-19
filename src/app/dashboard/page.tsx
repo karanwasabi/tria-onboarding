640ba42972f1668b9561a87f53472701c3b55cd7
Karan Wasabi <karan@karanwasabi.com>
2024-06-19 08:18:14 +0530

    feat: add crypto container

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,18 @@
+import CryptoContainer from '@/components/server/dashboard/crypto-container';
 import Header from '@/components/server/dashboard/header';
 import SummaryCard from '@/components/server/dashboard/summary-card';
 
 export default async function DashboardPage() {
   return (
     <>
-      <div className="relative flex aspect-square w-full flex-wrap items-start justify-center rounded-20 bg-t-black-gradient">
-        <video autoPlay loop muted playsInline className="absolute inset-0 z-10 h-full w-full object-cover opacity-40">
+      <div className="relative flex w-full flex-wrap items-start justify-center overflow-hidden rounded-20 bg-t-black-gradient">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute inset-0 z-10 aspect-square h-full w-full overflow-hidden object-cover opacity-40"
+        >
           <source src="/videos/logo-bg.mp4" type="video/mp4" />
         </video>
 
@@ -13,6 +20,10 @@ export default async function DashboardPage() {
           <Header />
           <SummaryCard />
         </div>
+      </div>
+
+      <div className="relative z-20 flex w-full grow flex-col items-center justify-start bg-t-dashboard-black px-4 py-5">
+        <CryptoContainer />
       </div>
     </>
   );
---
+++ src/components/server/dashboard/crypto-container/crypto-container.tsx
@@ -0,0 +1,13 @@
+const CryptoContainer: React.FC = () => {
+  return (
+    <>
+      <div className="flex w-full items-center justify-start rounded-xl">
+        <div className="text-16 font-semibold text-t-light-30">Crypto</div>
+      </div>
+    </>
+  );
+};
+
+CryptoContainer.displayName = 'CryptoContainer';
+
+export default CryptoContainer;
